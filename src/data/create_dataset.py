c1854b962772d0088aeeee2edd82c215eab6d9a0
vineet <vineetver@hotmail.com>
2022-05-06 02:40:56 -0400

    added get_output_file_name

--- src/data/create_dataset.py
+++ src/data/create_dataset.py
@@ -2,7 +2,10 @@
 Create_dataset.py
 Contains functions for importing and exporting data.
 """
+import os
 import pandas as pd
+
+BUCKET_NAME = 'yellowtaxi-bucket'
 
 
 def read_file(year: str, month: str) -> pd.DataFrame:
@@ -39,7 +42,56 @@ def get_file_path(year: str, month: str) -> str:
     assert isinstance(year, str), 'Year must be an string.'
     assert isinstance(month, str), 'Month must be an string.'
 
-    url = 'gs://yellowtaxi-bucket/'
-    file_path = url + 'yellow_tripdata_{}-{}.csv'.format(year, month)
+    url = f'gs://{BUCKET_NAME}/tripdata/'
+    file_path = f'{url}yellow_tripdata_{year}-{month}.csv'
 
     return file_path
+
+
+def write_file(df: pd.DataFrame, suffix: str, scratch: bool = True) -> str:
+    """
+    This function writes the dataframe to a csv file in the bucket
+
+    Args:
+        df: The data to write.
+        suffix: The suffix to add to the file name.
+        scratch: Write the file with the suffix
+
+    Returns:
+        None
+    """
+
+    assert suffix.endswith('.csv'), 'Suffix must end with .csv'
+
+    path = f'gs://{BUCKET_NAME}'
+
+    if scratch:
+        path = os.path.join(path, 'scratch')
+
+    path = os.path.join(path, suffix)
+
+    df.to_csv(path, index=False)
+
+    return path
+
+
+def get_output_file_name(stage: str, version: str = None) -> str:
+    """
+    This function returns the output file path for the given stage and version.
+
+    Args:
+        stage: The stage to write to.
+        version: The version of the stage to write to.
+
+    Returns:
+        The file path for the given stage and version.
+    """
+
+    output_file_name = f'{stage}'
+
+    if version is not None:
+        path = os.path.join(output_file_name, version)
+
+    path = os.path.join(output_file_name, stage)
+
+    return path
